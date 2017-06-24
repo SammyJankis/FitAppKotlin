c817b81335c5c343b66ea42e7ec03e014078de00
Arturo Guillen <sammyjankis87@gmail.com>
2017-06-24 10:31:56 +0200

    Initial commit

---
+++ app/src/main/java/com/sammyjankis/fitappkotlin/Constants.kt
@@ -0,0 +1,9 @@
+package com.sammyjankis.fitappkotlin
+
+/**
+ * Created by agl on 24/06/2017.
+ */
+
+class Constants {
+    val BASE_URL = "http://www.mocky.io"
+}
---
+++ app/src/main/java/com/sammyjankis/fitappkotlin/view/list/ListActivity.kt
@@ -0,0 +1,17 @@
+package com.sammyjankis.fitappkotlin.view.list
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by agl on 24/06/2017.
+ */
+
+class ListActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
